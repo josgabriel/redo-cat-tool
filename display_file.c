af1b787ca68a0bdfd98b9379444fb58ecf5211ff
josgabriel <50939382+josgabriel@users.noreply.github.com>
2021-11-28 01:20:10 -0300

    updated files

---
+++ display_file.c
@@ -0,0 +1,20 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define BUFFER 1000
+
+void ft_display_file(char *buf)
+{
+	int		fd;
+	char	conteudo[BUFFER];
+	int		s;
+
+	fd = open (buf, O_RDONLY);
+	s = read(fd, conteudo, BUFFER);
+	if (fd < 0 || s < 0)
+		write(2, "cannot read file\n", 17);
+	write(1, conteudo, s);
+
+	close(fd);
+}
